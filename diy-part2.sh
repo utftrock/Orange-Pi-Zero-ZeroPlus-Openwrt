f5e56c14a7771c11a2714355acb3c39a35fb45b9
utftrock <61354521+utftrock@users.noreply.github.com>
2024-10-08 22:54:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,18 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' feeds/luci/collections/luci/Makefile
-sed -i 's/KERNEL_PATCHVER=5.15/KERNEL_PATCHVER=5.4/g' target/linux/rockchip/Makefile
+# 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
+#sed -i 's/192.168.100.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
+
+# ttyd 自动登录
+# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
+
+# 添加自定义软件包
+# echo '
+# CONFIG_PACKAGE_luci-app-mosdns=y
+# CONFIG_PACKAGE_luci-app-adguardhome=y
+# CONFIG_PACKAGE_luci-app-openclash=y
+# ' >> .config
